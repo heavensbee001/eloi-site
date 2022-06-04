3b08ebe07e6a3c64bae9e7a4fe187c9829b1e0ff
heavensbee001 <heavensbee001@gmail.com>
2022-06-04 19:55:58 +0200

    Add some background art

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,7 +4,7 @@ import BurgerMenu from "./BurgerMenu";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
-        <section className="w-screen bg-orange min-h-screen bg-[url('/background_mobile.png')] bg-cover flex justify-center">
+        <section className="w-screen bg-orange min-h-screen bg-[url('/background2.png')] bg-cover bg-center flex justify-center overflow-hidden">
             <header className="fixed top-0 left-0 h-12 w-full pt-2 z-20 flex justify-center">
                 <div className="w-full max-w-screen-md text-center relative">
                     <Link href="/" passHref>
@@ -21,13 +21,23 @@ export default function Layout({ children }: { children: React.ReactNode }) {
                     <BurgerMenu />
                 </div>
             </header>
-            <div className="w-full max-w-screen-md">
+            <div className="w-full max-w-screen-md z-10">
                 <div className="h-20"></div>
                 <main className="h-5/6 w-full max-w-screen-md fixed overflow-y-scroll px-4">
                     {children}
                 </main>
                 <footer></footer>
             </div>
+            <section className="absolute w-full h-full top-0 left-0 overflow-hidden">
+                <span className="absolute -right-2/3 bottom-20 md right-32">
+                    <Image
+                        src="/cloud1.png"
+                        alt="cloud"
+                        width={600}
+                        height={300}
+                    />
+                </span>
+            </section>
         </section>
     );
 }
