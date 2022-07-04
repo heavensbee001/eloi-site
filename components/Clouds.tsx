a3056bb31fa02852b4b07adf3042327ddd39e79a
heavensbee001 <heavensbee001@gmail.com>
2022-07-04 13:04:27 +0200

    Fix marquee image blinking

--- components/Clouds.tsx
+++ components/Clouds.tsx
@@ -7,29 +7,20 @@ export default function Clouds() {
         <Fragment>
             <section className="absolute w-full bottom-48 md:bottom-36 left-0 overflow-hidden">
                 <Marquee gradient={false} speed={8}>
-                    <span style={{ paddingLeft: "100vw" }}>
-                        <Image
-                            src="/cloud1.png"
-                            alt="cloud"
-                            width={300}
-                            height={150}
-                        />
-                    </span>
+                    <div style={{ paddingLeft: "70vw" }}></div>
+                    <div>
+                        <img src="/cloud1.png" alt="cloud" width={300} />
+                    </div>
+                    <div style={{ paddingLeft: "30vw" }}></div>
                 </Marquee>
             </section>
             <section className="absolute w-full bottom-20 left-0 overflow-hidden">
                 <Marquee gradient={false} speed={20}>
-                    <span
-                        className="-scale-x-100"
-                        style={{ paddingLeft: "100vw" }}
-                    >
-                        <Image
-                            src="/cloud1.png"
-                            alt="cloud"
-                            width={500}
-                            height={300}
-                        />
-                    </span>
+                    <div style={{ paddingLeft: "30vw" }}></div>
+                    <div>
+                        <img src="/cloud1.png" alt="cloud" width={500} />
+                    </div>
+                    <div style={{ paddingLeft: "70vw" }}></div>
                 </Marquee>
             </section>
         </Fragment>
