59e6e7d6dda00bcce4cbbf123b000f4d1783a468
eloigil <eloi@joineden.ai>
2024-05-02 17:06:53 +0200

    Add some art changes

--- pages/art.tsx
+++ pages/art.tsx
@@ -3,45 +3,51 @@ import Card from "../components/Card";
 import Head from "next/head";
 
 export default function Projects() {
-    const router = useRouter();
+  const router = useRouter();
 
-    const cardsData: {
-        title: string;
-        subtitle: string;
-        img: string;
-        url: string;
-    }[] = [
-        {
-            title: "counting sheep",
-            subtitle: "",
-            img: "/projects/counting-sheep.png",
-            url: "https://editor.p5js.org/eloigil/sketches/l64gYtcm8",
-        },
-        {
-            title: "bamboo",
-            subtitle: "",
-            img: "/projects/bamboo.png",
-            url: "https://editor.p5js.org/eloigil/sketches/taZItGLEK",
-        },
-        {
-            title: "Durmitor",
-            subtitle: "",
-            img: "/projects/durmitor.png",
-            url: "https://editor.p5js.org/eloigil/sketches/3pSDq5XC2",
-        },
-    ];
+  const cardsData: {
+    title: string;
+    subtitle: string;
+    img: string;
+    url: string;
+  }[] = [
+    {
+      title: "Counting Sheep",
+      subtitle: "",
+      img: "/projects/counting-sheep.png",
+      url: "https://editor.p5js.org/eloigil/sketches/l64gYtcm8",
+    },
+    {
+      title: "Vall Nord",
+      subtitle: "",
+      img: "/projects/vallnord.png",
+      url: "https://editor.p5js.org/eloigil/sketches/2eI2qCjta",
+    },
+    {
+      title: "Bamboo",
+      subtitle: "",
+      img: "/projects/bamboo.png",
+      url: "https://editor.p5js.org/eloigil/sketches/taZItGLEK",
+    },
+    {
+      title: "Durmitor",
+      subtitle: "",
+      img: "/projects/durmitor.png",
+      url: "https://editor.p5js.org/eloigil/sketches/3pSDq5XC2",
+    },
+  ];
 
-    return (
-        <section>
-            <Head>
-                <title>eloi | art</title>
-            </Head>
-            <h2 className="font-sans text-white text-xl mb-6 ml-4">art</h2>
-            <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-                {cardsData.map((card, index) => (
-                    <Card {...card} key={index} />
-                ))}
-            </div>
-        </section>
-    );
+  return (
+    <section>
+      <Head>
+        <title>eloi | art</title>
+      </Head>
+      <h2 className="font-sans text-white text-xl mb-6 ml-4">art</h2>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+        {cardsData.map((card, index) => (
+          <Card {...card} key={index} />
+        ))}
+      </div>
+    </section>
+  );
 }
